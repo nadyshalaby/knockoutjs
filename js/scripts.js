96bfd476559d7083ad20fe08abb7bc61e71fc672
Nady Shalaby <nadyshalaby@gmail.com>
2022-02-14 07:46:58 +0200

    Commits

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,19 @@
-let viewModel = function () {
-    let self = this;
-
-    self.firstname = ko.observable("");
-    self.lastname = ko.observable("");
-
-    // ------( The following is a computed observable readable only. )------
-    //     return self.firstname() + " " + self.lastname();
-    // }),
-
-    // ------( The following is a computed observable readable and writable. )------
-    self.fullname = ko.computed({
-        read: function () {
-            return self.firstname() + " " + self.lastname();
-        },
-        write: function (value) {
-            let names = value.split(" ");
-            self.firstname(names[0]);
-            self.lastname(names[1]);
-        },
-    });
-
-    self.age = ko.observable(0);
-    self.ageInDogYears = ko.computed(function () {
-        return self.age() * 7;
-    });
-    self.isAdult = ko.computed(function () {
-        return self.age() >= 18;
-    });
-    self.isSenior = ko.computed(function () {
-        return self.age() >= 65;
-    });
-    self.isChild = ko.computed(function () {
-        return self.age() < 18;
-    });
-    self.isBaby = ko.computed(function () {
-        return self.age() < 1;
-    });
+let ViewModel = function (items) {
+    this.items = ko.observableArray(items);
+    this.itemToAdd = ko.observable("");
+    this.addItem = function () {
+        this.items.push(this.itemToAdd());
+        this.itemToAdd("");
+    }.bind(this);
 };
 
-ko.applyBindings(viewModel);
+ko.applyBindings(
+    new ViewModel([
+        "Introduction to JavaScript",
+        "Introduction to HTML and CSS",
+        "Introduction to jQuery",
+        "Introduction to Knockout.js",
+        "Introduction to Angular.js",
+        "Introduction to Node.js",
+    ])
+);
